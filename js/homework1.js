4e3f568289b0d9d70032eb9b954156a28062a87a
arinagalkina <arina.galkina1708@gmail.com>
2024-02-07 20:00:35 +0000

    Initial commit

---
+++ js/homework1.js
@@ -0,0 +1,40 @@
+const age = '14';
+
+console.log(age);
+
+
+
+const name = "Arina";
+
+console.log(name);
+
+
+
+
+const isStudent = confirm('Ви студент?');
+
+console.log(isStudent);
+
+
+
+const myString = (' "Успіх - це не випадковість, це результат постійного старання." - Наполеон Хілл. ');
+
+console.log(myString);
+
+
+let myNumber = 35;
+
+myNumber +=10;
+
+console.log(myNumber);
+
+const myNull = null;
+
+console.log(myNull);
+
+
+
+
+
+
+
