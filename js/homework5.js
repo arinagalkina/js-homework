47805d9b8230fa1a4f4a23d4558cc907a3a0d8f9
arinagalkina <arina.galkina1708@gmail.com>
2024-02-18 15:16:06 +0000

    homework 5

---
+++ js/homework5.js
@@ -0,0 +1,10 @@
+ const result = 5 + 5 + '5';
+console.log(typeof result);
+
+const nameSurname = 'Arina Galkina';
+console.log(nameSurname.length);
+
+const name = 'arina';
+console.log(name .toUpperCase());
+
+ console.log(nameSurname.indexOf( ));
